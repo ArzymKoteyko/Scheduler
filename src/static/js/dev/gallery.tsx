87e5e15f9723525e13e00873f053df117272de65
ArzymKoteyko <artemiy.kotenko@yandex.ru>
2023-02-17 18:16:49 +0300

    Created gallery

--- src/static/js/dev/gallery.tsx
+++ src/static/js/dev/gallery.tsx
@@ -12,6 +12,18 @@ export class Gallery extends React.Component<Props, State> {
     }   
 
     render() { return (<>
-        <h3>Gallery</h3>
+        <div className="Gallery"><div className="grid_contanier">
+            <div id="grid_box_1"></div>
+            <div id="grid_box_2"></div>
+            <div id="grid_box_3"></div>
+            <div id="grid_box_4"></div>
+            <div id="grid_box_5"></div>
+            <div id="grid_box_6"></div>
+            <div id="grid_box_7"></div>
+            <div id="grid_box_8"></div>
+            <div id="grid_box_9"></div>
+            <div id="grid_box_10"></div>
+            <div id="grid_box_11"></div>
+        </div></div>
     </>)}
 }
