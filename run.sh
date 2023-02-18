defac8ddf49c41336f00901c70db3e6d7a6b5448
ArzymKoteyko <artemiy.kotenko@yandex.ru>
2023-02-18 11:23:49 +0300

    fixed shell script

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
+python -m venv venv
+source ./venv/bin/activate
+pip install --upgrade pip 
+pip install -r requirements.txt
+npm install .
+npm run build
 export FLASK_APP=./src/index.py
-python3 -m flask run --host "0.0.0.0" --port 5000
+python -m flask run --host "0.0.0.0" --port 5000
